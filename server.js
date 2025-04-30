8f6ef843ff5716f436bbdd2edb0b5935c6ef4ce0
Shreycode28 <shrey.patel.s73@kalvium.community>
2025-04-30 16:52:34 +0530

    changes

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let signup_page = [];
+
+app.get("/users", (req, res) => {
+  res.json(signup_page);
+});
+
+app.post("/signup", (req, res) => {
+  const { username, email, password, data_of_birth } = req.body;
+
+  try {
+    if (!username) {
+      return res.json({ message: "Username  cannot be empty" });
+    } else if (!email) {
+      return res.json({ message: "Email cannot be empty" });
+    } else if (password.length < 8 || password.length > 16) {
+      return res.json({
+        message:
+          "Password length should be greater than 8 or less than or equal to 16",
+      });
+    }
+
+    const new_user = {
+      username,
+      email,
+      password,
+      data_of_birth,
+    };
+
+    signup_page.push(new_user);
+
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
